8798acfe5003cc1257e1e0d95bae0dc0b928c5a4
Alexander Davis <alexander.davis.098@gmail.com>
2022-12-06 21:27:39 -0300

    feat: added new auth routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
+import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
 
 const routes = Router();
 
+routes.use('/auth', authRoutes);
 routes.use('/users', userRoutes);
 
 export default routes;
