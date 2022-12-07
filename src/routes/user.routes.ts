3e0c35af489a63ebc95039d38e372023a2e19d6c
Alexander Davis <alexander.davis.098@gmail.com>
2022-12-06 21:28:33 -0300

    refactor: added hash password to user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from 'express';
 import { prisma } from '../lib/prisma';
 
+import bcrypt from 'bcrypt';
+
 import { validateUser } from '../helpers';
 
 const userRoutes = Router();
@@ -18,20 +20,29 @@ userRoutes.post('/', async (req: Request, res: Response) => {
         return res.status(400).json({ message: error?.message });
 
     try {
-        const user = await prisma.user.findUnique({
+        const userExists = await prisma.user.findUnique({
             where: {
                 email: value.email
             }
         });
-        
-        if (user)
+
+        if (userExists)
             return res.status(400).json({ message: 'This user already exists!' });
 
-        await prisma.user.create({
-            data: { name: value.name, email: value.email }
+        // Encrypting user password.
+        const hashPassword = await bcrypt.hash(value.password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                name: value.name,
+                email: value.email,
+                password: hashPassword
+            }
         });
 
-        return res.status(201).json();
+        const { id } = newUser;
+
+        return res.status(201).json({ id });
     } catch (error) {
         return res.status(500).json({ error: 'Unable to register. There was an internal server error.' });
     }
