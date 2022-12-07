815e2a5211b2fa0e92ac82076eb0f823908f25cc
Alexander Davis <alexander.davis.098@gmail.com>
2022-12-06 21:32:05 -0300

    refactor: added cors, cookie-parse, and url-enconded

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import routes from './routes';
 
@@ -9,8 +10,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+// Cross Origin Resource Sharing
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+// Middleware for cookies
+app.use(cookieParser());
+
+// Built-in middleware for json
 app.use(express.json());
-app.use(cors());
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/api/v1', routes);
