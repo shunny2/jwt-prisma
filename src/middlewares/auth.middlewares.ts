78d730ae1fe04af49ab77ef0265e7d98af303352
Alexander Davis <alexander.davis.098@gmail.com>
2022-12-24 10:27:37 -0300

    refactor: refactoring authentication middleware

--- src/middlewares/auth.middlewares.ts
+++ src/middlewares/auth.middlewares.ts
@@ -1,33 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
 import { prisma } from '../lib/prisma';
+import { verify } from 'jsonwebtoken';
 
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 import { ForbiddenError, UnauthorizedError } from '../helpers/api-errors';
 
 dotenv.config();
 
-type JWTPayload = {
-    id: string;
-    iat: number;
-    exp: number;
-}
-
 export const verifyToken = async (req: Request, _: Response, next: NextFunction) => {
-    const { authorization } = req.headers;
-
-    // Cut the received string and takes the token at position 1.
-    const token = authorization && authorization.split(' ')[1];
-
-    if (token == null)
-        throw new UnauthorizedError();
-
     try {
-        const { id } = jwt.verify(token, process.env.JWT_SECRET) as JWTPayload;
+        const { authorization } = req.headers;
 
-        const user = await prisma.user.findUnique({
-            where: { id }
+        // Cut the received string and takes the token at position 1.
+        const token = authorization && authorization.split(' ')[1] || '';
+
+        const payload: any = verify(token, process.env.JWT_SECRET);
+
+        if (!payload)
+            throw new UnauthorizedError();
+
+        const user = await prisma.user.findFirst({
+            where: {
+                id: payload.id
+            }
         });
 
         if (!user)
