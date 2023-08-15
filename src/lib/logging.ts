f737813bad63caf9683cb2abaeab04d5120b8a4a
Alexander Davis <alexander.davis.098@gmail.com>
2023-08-15 12:01:28 -0300

    style: switching from double quotes to single quotes

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -3,11 +3,11 @@ import chalk from 'chalk';
 export default class Logging {
     public static log = (args: any) => this.info(args);
     public static info = (args: any) => console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO]`),
-        typeof args === "string" ? chalk.blueBright(args) : args);
+        typeof args === 'string' ? chalk.blueBright(args) : args);
     public static success = (args: any) => console.log(chalk.green(`[${new Date().toLocaleString()}] [SUCCESS]`),
-        typeof args === "string" ? chalk.greenBright(args) : args);
+        typeof args === 'string' ? chalk.greenBright(args) : args);
     public static warn = (args: any) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [WARNING]`),
-        typeof args === "string" ? chalk.yellowBright(args) : args);
+        typeof args === 'string' ? chalk.yellowBright(args) : args);
     public static error = (args: any) => console.log(chalk.red(`[${new Date().toLocaleString()}] [ERROR]`),
-        typeof args === "string" ? chalk.redBright(args) : args);
+        typeof args === 'string' ? chalk.redBright(args) : args);
 }
