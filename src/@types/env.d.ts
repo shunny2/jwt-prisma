61a8b6549719f4f44fbe4560aed7aa690a3ac426
Alexander Davis <alexander.davis.098@gmail.com>
2023-11-01 22:02:37 -0300

    ♻️refactor: added base URL to environment types

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,6 +1,8 @@
 namespace NodeJS {
     interface ProcessEnv {
+        BASE_URL: string;
         PORT: number;
+
         JWT_SECRET: string;
         JWT_REFRESH_SECRET: string;
         EXPIRES_IN_JWT_SECRET: string | number;
