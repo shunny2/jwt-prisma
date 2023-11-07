df56987dff306cd857bb2bb73e5e0493fc1f87e1
Alexander Davis <alexander.davis.098@gmail.com>
2023-11-07 13:37:43 -0300

    ✨feat: added new email and password validations

--- src/helpers/validations.ts
+++ src/helpers/validations.ts
@@ -32,3 +32,21 @@ export const validateSignIn = (user: IUser) => {
 
     return schema.validate(user, { presence: 'required' });
 };
+
+export const validateEmail = (user: IUser) => {
+    const schema = Joi.object<IUser>({
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    });
+
+    return schema.validate(user, { presence: 'required' });
+};
+
+export const validatePassword = (user: IUser) => {
+    const schema = Joi.object<IUser>({
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{8,30}$'))
+    });
+
+    return schema.validate(user, { presence: 'required' });
+};
