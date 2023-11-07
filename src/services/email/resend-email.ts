af815f1ec09f8fb15eb1d2679a423fae9d245335
Alexander Davis <alexander.davis.098@gmail.com>
2023-11-07 13:30:30 -0300

    ✨feat: added a new function to resend emails

---
+++ src/services/email/resend-email.ts
@@ -0,0 +1,52 @@
+import { User } from '@prisma/client';
+
+import { prisma } from '../../lib/prisma';
+import { emailToken } from '../../utils/email-token';
+import { sendEmail } from './send-email';
+import { IMessage } from '../../interfaces/email';
+
+/**
+ * This function is responsible for sending confirmation emails when the token has expired.
+ * @param user User
+ * @param token string
+ * @returns Promise
+ */
+export const resendEmail = async (user: User, token: string) => {
+    try {
+        // Deletes the expired token.
+        await prisma.token.delete({
+            where: {
+                token
+            }
+        });
+
+        const newConfirmationToken = emailToken.generateNewToken();
+
+        await prisma.token.create({
+            data: {
+                userId: user.id,
+                token: newConfirmationToken,
+                type: 'confirmationEmail',
+                expiredAt: emailToken.generateExpirationDate()
+            }
+        });
+
+        const url = `${process.env.BASE_URL}:${process.env.PORT}/api/v1/users/email-confirmation/${token}`;
+
+        const message: IMessage = {
+            to: {
+                name: user.name,
+                email: user.email,
+            },
+            subject: 'Confirmation email',
+            template: {
+                name: 'confirm-email',
+                url
+            }
+        }
+
+        await sendEmail(message);
+    } catch (error: any) {
+        throw new Error('An error occurred when trying to resend the confirmation email.');
+    }
+};
