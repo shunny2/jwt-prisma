0c9a3ea1798ca07117e6f67fb222f29ab785722d
Alexander Davis <alexander.davis.098@gmail.com>
2023-11-07 12:53:50 -0300

    🔧chore: added new environment variables for the email service

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -7,5 +7,12 @@ namespace NodeJS {
         JWT_REFRESH_SECRET: string;
         EXPIRES_IN_JWT_SECRET: string | number;
         EXPIRES_IN_JWT_REFRESH_SECRET: string | number;
+
+        HOST: string;
+        SERVICE: string;
+        EMAIL_PORT: number;
+        SECURE: boolean;
+        USERNAME: string;
+        PASSWORD: string;
     }
 }
